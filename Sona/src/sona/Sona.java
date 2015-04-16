ad9f6079b806d77068fb316767d093b05a4cfad9
a1320726 <a1320726@L2-03.HTO.IFSP.EDU.BR>
2015-04-16 11:14:07 -0300

    (Dia 1) Inicio do projeto ..

---
+++ Sona/src/sona/Sona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sona;
+
+/**
+ *
+ * @author a1320726
+ */
+public class Sona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
