a118fa1907a5b6d4a85199c2864d6a0fc9cb51dd
Luiz <Luiz@192.168.0.161>
2015-06-26 19:38:50 -0300

    Criado Usuario, telas ainda não implementada, falta o DAO do usuario

---
+++ Sona/src/Model/Usuario.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Luiz
+ */
+@Entity
+public class Usuario {
+
+    public Usuario() {
+    
+    }
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    int id;
+    String login;
+    String senha;
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+    
+    
+}
