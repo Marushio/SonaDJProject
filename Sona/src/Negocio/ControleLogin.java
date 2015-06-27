a260ff26635e15cd6fd8f3749fa92290e1628f62
Luiz <Luiz@192.168.0.137>
2015-06-26 22:53:49 -0300

    ControleLogin, IControleLogin e NegocioFactory terminados

--- Sona/src/Negocio/ControleLogin.java
+++ Sona/src/Negocio/ControleLogin.java
@@ -5,6 +5,10 @@
  */
 package Negocio;
 
+import Model.Usuario;
+import Persistencia.IUsuarioDAO;
+import Persistencia.PersistenciaFactory;
+
 /**
  *
  * @author Luiz
@@ -14,10 +18,31 @@ public class ControleLogin implements IControleLogin{
     ControleLogin() {
         
     }
-    
-    public boolean logar(String login, String senha) {
-        boolean estado = false;
-        return estado;
+
+    @Override
+    public Usuario logar(String login, String senha) {
+        IUsuarioDAO usuarioDAO = PersistenciaFactory.obterUsuarioDAO();
+        Usuario usuario = usuarioDAO.obterUsuario(login);
+        if(senha.equals(usuario.getSenha()))
+            return usuario;
+        return null;
     }
+
+    @Override
+    public boolean cadastrar(String login, String senha) {
+        Usuario usuario = new Usuario();
+        IUsuarioDAO usuarioDAO = PersistenciaFactory.obterUsuarioDAO();
+        if(usuarioDAO.obterUsuario(login)==null){
+            usuario.setLogin(login);
+            usuario.setSenha(senha);
+            usuarioDAO.novoUsuario(usuario);
+            return true;
+        }else{
+            System.out.println("Usuário já cadastrado");
+            return false;
+        }
+            
+    }
+    
     
 }
