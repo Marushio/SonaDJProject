0fc60053c9cdb5e4ce0e449d4ce3f022decd1f3a
Marushio <marcio.olivercross@gmail.com>
2015-06-27 22:20:01 -0300

    implenetenddo tela nova musica

--- Sona/src/Negocio/ControleTelaPlayList.java
+++ Sona/src/Negocio/ControleTelaPlayList.java
@@ -6,14 +6,41 @@
 package Negocio;
 
 
+import Persistencia.IArtistaDAO;
 import Persistencia.PersistenciaFactory;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  *
  * @author Marcio
  */
 public class ControleTelaPlayList implements IControleTelaPlayList{
+    public Collection obterArtistas() {        
+        Collection artistas = new ArrayList(); 
+     
+        IArtistaDAO artistaDAO = PersistenciaFactory.obterArtistaDAO();
+        //marcasDisponiveis = marcaDAO.obterMarcasCadastrados();
+        return artistas;   
+    }
+    public Collection obterAlbuns(String artista) {
+        Collection modelosDisponiveis=new ArrayList();
+        persistenciaFactory =  new PersistenciaFactory();
+        IModeloDAO modeloDAO = persistenciaFactory.obterModeloDAO();
+        if(marca.equals("Todos")) {
+            modelosDisponiveis = modeloDAO.obterModelosCadastrados();
+        } else {
+            Collection modelosCadastrados = modeloDAO.obterModelosCadastrados();
+            Iterator i = modelosCadastrados.iterator();
+            while(i.hasNext()) {
+                Modelo m = (Modelo) i.next();
+                if (m.getMarca().getNome().equals(marca)) {
+                    modelosDisponiveis.add(m);
+                }
+            }
+        }
+        return modelosDisponiveis;
+    }    
     
 }
